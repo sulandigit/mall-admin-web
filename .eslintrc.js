f35c35335cbec1e2cb3f45b27a3e6cf4c0c639ee
QoderAI <qoder_ai@qoder.com>
2025-09-25 12:55:53 +0000

    chore: optimize package.json
    Co-authored-by: landi su <sulandi@qq.com>

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-unused-vars': 'error',
+    'vue/require-v-for-key': 'error',
+    'vue/no-use-v-if-with-v-for': 'error'
+  }
+}
